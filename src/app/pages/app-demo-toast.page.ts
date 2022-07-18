a7fbb4d5ee3411e2ada38f2731532584d2ef8eb1
Antony <butterspower@gmail.com>
2022-07-18 23:56:10 +0300

    fixes

--- src/app/pages/app-demo-toast.page.ts
+++ src/app/pages/app-demo-toast.page.ts
@@ -9,11 +9,11 @@ export class AppDemoToast extends LitElement {
 
 	override render() {
 		return html`
-			<sl-button @click=${() => notify('Hello', 'primary', 'info-circle')} variant="primary">Primary</sl-button>
-			<sl-button @click=${() => notify('Hello', 'success', 'check2-circle')} variant="success">Success</sl-button>
-			<sl-button @click=${() => notify('Hello', 'neutral', 'gear')} variant="neutral">Neutral</sl-button>
-			<sl-button @click=${() => notify('Hello', 'warning', 'exclamation-triangle')} variant="warning">Warning</sl-button>
-			<sl-button @click=${() => notify('Hello', 'danger', 'exclamation-octagon')} variant="danger">Danger</sl-button>
+			<sl-button @click=${() => notify('Hello', 'primary', 3000, 'info-circle')} variant="primary">Primary</sl-button>
+			<sl-button @click=${() => notify('Hello', 'success', 3000, 'check2-circle')} variant="success">Success</sl-button>
+			<sl-button @click=${() => notify('Hello', 'neutral', 3000, 'gear')} variant="neutral">Neutral</sl-button>
+			<sl-button @click=${() => notify('Hello', 'warning', 3000, 'exclamation-triangle')} variant="warning">Warning</sl-button>
+			<sl-button @click=${() => notify('Hello', 'danger', 3000, 'exclamation-octagon')} variant="danger">Danger</sl-button>
 		`
 	}
 }
