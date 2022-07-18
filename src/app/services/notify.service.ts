04646fa6f906980731e72a5ad8115c7f7a08f1fd
Antony <butterspower@gmail.com>
2022-07-18 23:52:49 +0300

    some improvements to notify

--- src/app/services/notify.service.ts
+++ src/app/services/notify.service.ts
@@ -1,10 +1,15 @@
-export const notify = async (message: string, variant = 'primary', icon = 'info-circle', duration = 3000) => {
+export const notify = async (
+	message: string,
+	variant: 'primary' | 'success' | 'neutral' | 'warning' | 'danger' = 'primary',
+	duration = 3000,
+	icon = 'info-circle',
+) => {
 	await import('@shoelace-style/shoelace/dist/components/alert/alert.js')
 
 	const alert = Object.assign(document.createElement('sl-alert'), {
 		variant,
 		closable: true,
-		duration: duration,
+		duration,
 		innerHTML: `
             <sl-icon name="${icon}" slot="icon"></sl-icon>
             ${message}
