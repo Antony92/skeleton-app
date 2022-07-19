0b826280469098289f433b76e38017b304c57796
Antony <butterspower@gmail.com>
2022-07-19 23:12:48 +0300

    fix api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,8 @@
 import { toQueryParams } from '../utils/http'
 
-export const getProducts = async () => {
+export const getProducts = async (search?: string, limit = 10) => {
 	try {
-		const req = await fetch(`https://dummyjson.com/products?limit=10`)
+		const req = await fetch(`https://dummyjson.com/products${toQueryParams({ q: search, limit })}`)
 		const res = await req.json()
 		return res?.products ?? []
 	} catch (error) {
@@ -16,17 +16,6 @@ export const getUsers = async (skip?: number, limit = 10) => {
 		const req = await fetch(`https://dummyjson.com/users${toQueryParams({ skip, limit })}`)
 		const res = await req.json()
 		return res
-	} catch (error) {
-		console.error(error)
-		return []
-	}
-}
-
-export const searchForProducts = async (search?: string) => {
-	try {
-		const req = await fetch(`https://dummyjson.com/products/search${search ? `?q=${search}` : ``}`)
-		const res = await req.json()
-		return res?.products ?? []
 	} catch (error) {
 		console.error(error)
 		return []
