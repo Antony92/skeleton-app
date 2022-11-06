d4545d2e48cab4992a1167bd53a506d05373a20b
Antony <butterspower@gmail.com>
2022-11-06 21:38:08 +0200

    fix user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,18 @@
 import { BehaviorSubject, shareReplay } from 'rxjs'
 
-const $user = new BehaviorSubject<any>(null)
+const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null
+
+const $user = new BehaviorSubject<any>(user)
 
 const userObservable = $user.asObservable().pipe(shareReplay(1))
 
 export const setUser = (user: any) => {
+	localStorage.setItem('user', JSON.stringify(user))
 	$user.next(user)
 }
 
 export const removeUser = () => {
+	localStorage.removeItem('user')
 	$user.next(null)
 }
 
