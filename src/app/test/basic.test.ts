81fbe9f318d4f360fcd02d6855f1f54e44e01984
Antony <butterspower@gmail.com>
2023-01-06 22:05:16 +0200

    test fixes

--- src/app/test/basic.test.ts
+++ src/app/test/basic.test.ts
@@ -4,13 +4,13 @@ import { toQueryParams } from '../utils/http'
 
 test('escapeHtml(html: string)', () => {
     const escapedHtml = escapeHtml('<script>alert(1)</script>')
-    expect(escapedHtml).to.not.include('<')
-    expect(escapedHtml).to.not.include('>')
+    expect(escapedHtml).not.include('<')
+    expect(escapedHtml).not.include('>')
 })
 
 test('toQueryParams(params: { [key: string]: any })', () => {
     const queryParams = toQueryParams({ limit: 10, search: 'user' })
-    expect(queryParams).to.contain('?')
-    expect(queryParams).to.contain('limit=10')
-    expect(queryParams).to.contain('search=user')
+    expect(queryParams).toContain('?')
+    expect(queryParams).toContain('limit=10')
+    expect(queryParams).toContain('search=user')
 })
