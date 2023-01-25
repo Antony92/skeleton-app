be84a5a570c924633d2b0901ec7bb11642d0a809
Antony92 <butterspower@gmail.com>
2023-01-25 11:10:34 +0200

    fixes

--- src/app/styles/table.style.ts
+++ src/app/styles/table.style.ts
@@ -7,30 +7,6 @@ export const tableWrapperStyle = css`
     }
 `
 
-export const tableLoaderStyle = css`
-    .table-loader {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        z-index: 1;
-        visibility: visible;
-        opacity: 1;
-        transition: all 0.3s;
-    }
-
-    .table-loader sl-spinner {
-        font-size: 3rem;
-    }
-
-    .table-loader[hidden] {
-        opacity: 0;
-        visibility: hidden;
-    }
-`
-
 export const tableStyle = css`
     table {
         width: 100%;
@@ -56,18 +32,5 @@ export const tableStyle = css`
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
-    }
-
-    table thead th.sortable {
-        cursor: pointer;
-    }
-
-    table thead th.sortable sl-icon {
-        position: relative;
-        top: 3px;
-    }
-
-    table thead :is(sl-input, sl-select) {
-        min-width: 200px;
     }
 `
