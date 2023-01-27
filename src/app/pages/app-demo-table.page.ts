94494165e1faeddbc6fbe3b8a350e23c7fc05d7b
Antony <butterspower@gmail.com>
2023-01-27 16:03:08 +0200

    fix

--- src/app/pages/app-demo-table.page.ts
+++ src/app/pages/app-demo-table.page.ts
@@ -86,6 +86,9 @@ export class AppDemoTable extends LitElement {
 		})
 	}
 
+	firstUpdated() {
+		this.paginator.pageIndex = this.#skip / this.#limit
+	}
 
 	async loadUsers() {
 		this.loading = true
