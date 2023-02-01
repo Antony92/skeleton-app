b3a839f8bba336de60372463554bf586b37dae71
Antony <butterspower@gmail.com>
2023-02-01 23:26:15 +0200

    fix

--- src/app/utils/url.ts
+++ src/app/utils/url.ts
@@ -1,21 +1,21 @@
 import { SearchParams } from "../types/search.type"
 
 export const addSearchParamsToURL = (searchParams: SearchParams) => {
-    const params = new URLSearchParams()
+    const search = new URLSearchParams()
 	Object.keys(searchParams)
 		.filter((key) => searchParams[key] != null && searchParams[key] != undefined && searchParams[key] !== '')
 		.map((key) => searchParams.set(key, searchParams[key].toString()))
-    const query = params.toString()
+    const query = search.toString()
     const url = `${window.location.pathname}` + `${query ? `?${query}` : ''}`  
     history.replaceState(null, '', url)
 }
 
 export const searchParamsToURL = (searchParams: SearchParams) => {
-	const params = new URLSearchParams()
-	Object.keys(params)
+	const search = new URLSearchParams()
+	Object.keys(searchParams)
 		.filter((key) => searchParams[key] != null && searchParams[key] != undefined && searchParams[key] !== '')
-		.map((key) => params.append(key, searchParams[key].toString()))
-	const query = searchParams.toString()
+		.map((key) => search.append(key, searchParams[key].toString()))
+	const query = search.toString()
 	return query ? `?${query}` : ``
 }
 
