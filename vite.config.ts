b64ae5d0484e480307d8f29d7975a009eeb9955a
Antony <butterspower@gmail.com>
2023-02-01 20:23:51 +0200

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     build: {
         target: 'esnext'
     },
+    plugins: [
+        VitePWA({ registerType: 'autoUpdate' })
+    ]
 })
