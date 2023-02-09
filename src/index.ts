a01adc882d0a374367b10e794397998400386ed7
Antony <butterspower@gmail.com>
2023-02-09 22:36:10 +0200

    added initial loading

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-// Import root element
+// service worker
+import { registerSW } from 'virtual:pwa-register'
+
+// register service worker
+registerSW({ immediate: true })
+
+// import root element
 import './app/app-root.element'
 
-// Theme
+// theme
 import { applyTheme } from './app/utils/theme'
 
 // set up theme
 applyTheme()
 
 // when root is loaded add ready to body
-Promise.allSettled([customElements.whenDefined('app-root')]).then(() => document.querySelector('body')?.classList.add('ready'))
+Promise.allSettled([customElements.whenDefined('app-root')]).then(() => {
+    document.querySelector('body')?.classList.remove('loading')
+    document.querySelector('.loader')?.remove()
+})
