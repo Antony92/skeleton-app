0c7c79e072d8d6b75d8addf82dfa2463fc220941
Antony <butterspower@gmail.com>
2023-02-13 21:46:26 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,18 @@ import { registerSW } from 'virtual:pwa-register'
 // register service worker
 registerSW({ immediate: true })
 
-// import root element
-import './app/app-root.element'
-
 // theme
 import { applyTheme } from './app/utils/theme'
 
 // set up theme
 applyTheme()
 
-// when root is loaded add ready to body
-Promise.allSettled([customElements.whenDefined('app-root')]).then(() => {
+// when root is hide loading 
+Promise.allSettled([
+    import('./app/app-root.element'),
+    customElements.whenDefined('app-root')
+]).then(() => {
     document.querySelector('body')?.classList.remove('loading')
     document.querySelector('.loader')?.remove()
 })
+
