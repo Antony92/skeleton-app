4f2f521589bfa5f724f5e33e62bc619caed69ffd
Antony92 <butterspower@gmail.com>
2023-11-17 18:03:15 +0200

    fixes

---
+++ src/app/services/feedback.service.ts
@@ -0,0 +1,17 @@
+import { request } from '../http/request'
+
+export const submitFeedback = async (feedback: { satisfaction: number; message: string }) => {
+	try {
+		const req = await request(`${import.meta.env.VITE_API}/feedback`, {
+			method: 'POST',
+			auth: true,
+			json: true,
+			body: JSON.stringify(feedback),
+		})
+		const res = await req.json()
+		return res
+	} catch (error) {
+		console.error(error)
+	}
+	return null
+}
