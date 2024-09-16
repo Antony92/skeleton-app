aa46306d7c1e6292806cab9f0450b0d2a3112839
Antony92 <butterspower@gmail.com>
2024-09-16 15:56:37 +0300

    added code to remember

---
+++ src/app/elements/table/custom-event.event.ts
@@ -0,0 +1,16 @@
+// export class SlSelectEvent extends Event {
+//     readonly item: MenuItem;
+  
+//     constructor(item: MenuItem) {
+//       super('sl-select', {bubbles: true});
+//       this.item = item;
+//     }
+//   }
+  
+//   declare global {
+//     interface GlobalEventHandlersEventMap {
+//       'sl-select': SlSelectEvent;
+//     }
+//   }
+
+// this.dispatchEvent(new SelectEvent(item));
