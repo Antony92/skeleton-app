581c949af3606a7452edaf8a4bf3add10bc9b408
Antony92 <butterspower@gmail.com>
2024-11-27 23:12:45 +0200

    fix

--- src/app/pages/app-profile.page.ts
+++ src/app/pages/app-profile.page.ts
@@ -1,11 +1,17 @@
+import { PreventCommands, Router, RouterLocation, WebComponentInterface } from '@vaadin/router'
 import { html, LitElement, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 @customElement('app-profile')
-export class AppProfile extends LitElement {
+export class AppProfile extends LitElement implements WebComponentInterface {
 	static styles = [
 		css``
 	]
+
+	onBeforeLeave(location: RouterLocation, commands: PreventCommands, router: Router) {
+        alert(`about to leave ${location.pathname}`)
+        return commands.prevent()
+    }
 
 	render() {
 		return html`Profile`
