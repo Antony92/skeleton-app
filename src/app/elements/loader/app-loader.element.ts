d9caa6829bc2a4f135622994d7756fe13a30a408
Antony92 <butterspower@gmail.com>
2024-12-15 13:50:48 +0200

    fix

--- src/app/elements/loader/app-loader.element.ts
+++ src/app/elements/loader/app-loader.element.ts
@@ -12,7 +12,7 @@ export class AppLoader extends LitElement {
 			left: 0;
 			right: 0;
 			bottom: 0;
-			z-index: 999;
+			z-index: var(--layer-important);
 			display: grid;
 			cursor: progress;
 		}
