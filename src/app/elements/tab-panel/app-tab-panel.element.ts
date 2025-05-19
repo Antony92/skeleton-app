e90f0f7940cd05b8a38aad16e3c0309c32552d73
Antony92 <butterspower@gmail.com>
2025-05-19 14:53:34 +0300

    fix

--- src/app/elements/tab-panel/app-tab-panel.element.ts
+++ src/app/elements/tab-panel/app-tab-panel.element.ts
@@ -1,27 +1,32 @@
 import { html, LitElement, css } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import { when } from 'lit/directives/when.js'
 
 @customElement('app-tab-panel')
 export class AppTabPanel extends LitElement {
-	static styles = [css``]
+	static styles = [
+		css`
+			:host {
+				display: none;
+			}
+
+			:host([active]) {
+				display: block;
+				width: 100%;
+			}
+		`,
+	]
 
 	@property({ type: Boolean })
 	active = false
 
 	@property({ type: String })
 	name = ''
-	
+
 	render() {
 		return html`
-			${when(
-				this.active,
-				() => html`
-					<div part="panel">
-						<slot></slot>
-					</div>
-				`
-			)}
+			<div part="panel">
+				<slot></slot>
+			</div>
 		`
 	}
 }
