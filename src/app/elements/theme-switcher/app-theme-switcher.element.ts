57af3dcc3ca40dd9650ca720701b7be0aa83344c
Antony92 <butterspower@gmail.com>
2026-01-22 15:46:25 +0200

    update theme switcher

--- src/app/elements/theme-switcher/app-theme-switcher.element.ts
+++ src/app/elements/theme-switcher/app-theme-switcher.element.ts
@@ -1,5 +1,11 @@
 import { html, LitElement, css } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import '@app/elements/dropdown/app-dropdown.element'
+import '@app/elements/dropdown-item/app-dropdown-item.element'
+import '@app/elements/button/app-button.element'
+import '@app/elements/icon/app-icon.element'
+import type { AppSelectEvent } from '@app/events/select.event'
+import { choose } from 'lit/directives/choose.js'
 
 @customElement('app-theme-switcher')
 export class AppThemeSwitcher extends LitElement {
@@ -11,9 +17,8 @@ export class AppThemeSwitcher extends LitElement {
 	preferedDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 	preferedLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches
 
-	changeTheme(event: Event) {
-		const value = (event.target as HTMLSelectElement).value
-		this.theme = value
+	changeTheme(event: AppSelectEvent) {
+		this.theme = event.value
 		localStorage.setItem('theme', this.theme)
 
 		const body = document.querySelector('body')!
@@ -31,11 +36,29 @@ export class AppThemeSwitcher extends LitElement {
 
 	render() {
 		return html`
-			<select @change=${this.changeTheme} .value=${this.theme}>
-				<option value="auto">Auto</option>
-				<option value="light">Light</option>
-				<option value="dark">Dark</option>
-			</select>
+			<app-dropdown @app-select=${this.changeTheme} title="Choose theme">
+				<app-button slot="trigger" icon>
+					<app-icon filled>
+						${choose(this.theme, [
+							['light', () => html`light_mode`],
+							['dark', () => html`dark_mode`],
+							['auto', () => html`contrast`],
+						])}
+					</app-icon>
+				</app-button>
+				<app-dropdown-item value="light">
+					<app-icon slot="prefix">light_mode</app-icon>
+					Light theme
+				</app-dropdown-item>
+				<app-dropdown-item value="dark">
+					<app-icon slot="prefix">dark_mode</app-icon>
+					Dark theme
+				</app-dropdown-item>
+				<app-dropdown-item value="auto">
+					<app-icon slot="prefix">contrast</app-icon>
+					Device default
+				</app-dropdown-item>
+			</app-dropdown>
 		`
 	}
 }
